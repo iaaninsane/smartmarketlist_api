5ff74c20f823131a01ba4a30963bf68cf86d4e67
IvanSF <iaaninsane@gmail.com>
2023-12-16 18:08:03 -0300

    mapeamento orm da classe ItemDaListaEntity

--- src/main/java/com/smartmarketlist/SmartMarketListApi/Entities/ItemDaListaEntity.java
+++ src/main/java/com/smartmarketlist/SmartMarketListApi/Entities/ItemDaListaEntity.java
@@ -1,9 +1,13 @@
 package com.smartmarketlist.SmartMarketListApi.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,8 +18,13 @@ public class ItemDaListaEntity {
 	private Long id;
 	private Integer qtd; 
 	
+	@ManyToOne
+	@JoinColumn(name="produto_id")
 	private ProdutoEntity produtoEntity;
 	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name="lista_id")
 	private ListaProdutosEntity listaProdutosEntity;
 
 	
