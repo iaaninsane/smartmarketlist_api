db173e9548d02e71d4ac88c64c3d7a171c50feee
IvanSF <iaaninsane@gmail.com>
2024-08-08 21:27:39 -0300

    ajuste dos atributos das entidades e incluão de dependênbcias no pom

--- src/main/java/listaprodutos/ListaProdutosController.java
+++ src/main/java/listaprodutos/ListaProdutosController.java
@@ -1,7 +1,19 @@
 package listaprodutos;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/listas")
 public class ListaProdutosController {
+	
+	private ListaProdutosService listaProdutosService;
 
+	public ListaProdutosController(ListaProdutosService listaProdutosService) {
+		
+		this.listaProdutosService = listaProdutosService;
+	}
+
+	
 }
---
+++ src/main/java/listaprodutos/ListaProdutosService.java
@@ -0,0 +1,5 @@
+package listaprodutos;
+
+public class ListaProdutosService {
+
+}
